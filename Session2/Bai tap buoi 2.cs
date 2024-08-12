3512f6ea39938d6064b1e7e334b4ae1048bc126c
zypaoo <baonguyen.31231023455@st.ueh.edu.vn>
2024-08-13 02:50:47 +0700

    modified

--- Session2/Bai tap buoi 2.cs
+++ Session2/Bai tap buoi 2.cs
@@ -1,13 +1,19 @@
-﻿internal class Program
+﻿using System.Formats.Asn1;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        /*bai1();
+        bai1();
         bai2();
         bai3();
         bai4();
-        bai5();*/
+        bai5();
+        bai6();
         bai8();
+        bai9();
+        bai10();
+        
     }
     static void bai1()
     {
@@ -61,18 +67,47 @@
         }
         else
         {
-            Console.WriteLine("Khong thoa man");
+            Console.WriteLine("Khong thoa man yeu cau");
         }
 
     }
+    static void bai6()
+    {
+        Console.WriteLine("Kich thuoc cua kieu bool la: " + sizeof(bool) + " bytes");
+        Console.WriteLine("Kich thuoc cua kieu byte la: " + sizeof(byte) + " bytes");
+        Console.WriteLine("Kich thuoc cua kieu char la: " + sizeof(char) + " bytes");
+        Console.WriteLine("Kich thuoc cua kieu decimal la: " + sizeof(decimal) + " bytes");
+        Console.WriteLine("Kich thuoc cua kieu double la: " + sizeof(double) + " bytes");
+        Console.WriteLine("Kich thuoc cua kieu float la: " + sizeof(float) + " bytes");
+        Console.WriteLine("Kich thuoc cua kieu int la: " + sizeof(int) + " bytes");
+        Console.WriteLine("Kich thuoc cua kieu long la: " + sizeof(long) + " bytes");
+        Console.WriteLine("Kich thuoc cua kieu sbyte la: " + sizeof(sbyte) + " bytes");
+        Console.WriteLine("Kich thuoc cua kieu short la: " + sizeof(short) + " bytes");
+        Console.WriteLine("Kich thuoc cua kieu unit la: " + sizeof(uint) + " bytes");
+        Console.WriteLine("Kich thuoc cua kieu ulong la: " + sizeof(ulong) + " bytes");
+        Console.WriteLine("Kich thuoc cua kieu ushort la: " + sizeof(ushort) + " bytes");
+
+    }
+    //bỏ bài 7
     static void bai8()
     {
         Console.Write("Nhap vao ban kinh r: ");
         double r = Convert.ToDouble(Console.ReadLine());
         Console.WriteLine("Dien tich hinh tron la: " + (r * r * 3.14));
-
     }
-
+    static void bai9()
+    {
+        Console.Write("Nhap vao canh a9: ");
+        double r= Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Dien tich hinh vuong la: " + (r * r));
+    }
+    static void bai10()
+    {
+        Console.Write("Nhap vao so ngay can chuyen: ");
+        int a10 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine($" Ta co: {a10 / 365} nam va {a10 % 365} ngay ");
+        Console.WriteLine($" Ta co: {a10 / 7} tuan va {a10 % 7} ngay");
+    }
 }
 
 
